6e80cf55898d177f95098d1614c10292ce4c404f
Vinicius Gonçalves Martins <vm3090474@gmail.com>
2021-10-09 22:07:25 -0300

    Solução do projeto

---
+++ certificacao/Aula1 - criar tipos de valor/TiposDeValor.cs
@@ -0,0 +1,12 @@
+namespace certificacao.Aula1___criar_tipos_de_valor
+{
+    public class TiposDeValor
+    {
+        public void Executar() {
+
+            int idade = 30;
+
+            Console.WriteLine(idade);
+        }
+    }
+}
