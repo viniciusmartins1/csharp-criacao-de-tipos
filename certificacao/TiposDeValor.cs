098c34f1829051021cb551d73956c5b88b2c58f1
Vinicius Gonçalves Martins <vm3090474@gmail.com>
2021-10-11 23:05:20 -0300

    Características do tipo de valor

--- certificacao/TiposDeValor.cs
+++ certificacao/TiposDeValor.cs
@@ -14,6 +14,19 @@ namespace certificacao
             int idade = 30;
 
             Console.WriteLine(idade);
+
+            int copiaIdade = idade;
+
+            Console.WriteLine("idade: {0}", idade);
+            Console.WriteLine("idade: {0}", copiaIdade);
+
+            idade = 23;
+
+            Console.WriteLine("idade: {0}", idade);
+            Console.WriteLine("idade: {0}", copiaIdade);
+
+            int? idade2 = null;
+
         }
     }
 }
